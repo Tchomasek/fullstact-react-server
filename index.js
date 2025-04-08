7a2cd03f9f385201165675e29b8255d0569e6089
Tchomasek <tchom.asek@centrum.cz>
2025-04-08 08:19:03 +0200

    back to start

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
@@ -11,12 +10,6 @@ const db = require("./models");
 // Routers
 const postRouter = require("./routes/Posts");
 app.use("/posts", postRouter);
-const commentsRouter = require("./routes/Comments");
-app.use("/comments", commentsRouter);
-const usersRouter = require("./routes/Users");
-app.use("/auth", usersRouter);
-const likesRouter = require("./routes/Likes");
-app.use("/likes", likesRouter);
 
 db.sequelize
   .sync()
